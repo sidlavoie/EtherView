f6ee00ebdede917c2cc97dfc5346aa93ae6b6743
sidlavoie <76972727+sidlavoie@users.noreply.github.com>
2022-01-29 13:08:51 -0500

    v0.4
    added GUI to the main.py

--- EtherView/lib.py
+++ EtherView/lib.py
@@ -4,7 +4,8 @@ import struct
 import subprocess
 import speedtest
 
-def get_ip(iface): #Gets the Raspi's IPv4 address
+
+def get_ip(iface):  # Gets the Raspi's IPv4 address
     try:
         address = netifaces.ifaddresses(iface)
         ip_addr = address[netifaces.AF_INET][0]['addr']
@@ -15,7 +16,7 @@ def get_ip(iface): #Gets the Raspi's IPv4 address
         return "N/A"
 
 
-def get_netmask(iface): #Gets the Raspi's netmask in the 255.255.255.0 format
+def get_netmask(iface):  # Gets the Raspi's netmask in the 255.255.255.0 format
     try:
         address = netifaces.ifaddresses(iface)
         netmask = address[netifaces.AF_INET][0]['netmask']
@@ -26,7 +27,7 @@ def get_netmask(iface): #Gets the Raspi's netmask in the 255.255.255.0 format
         return "N/A"
 
 
-def get_default_gateway(): #Gets the Raspi's default gateway
+def get_default_gateway():  # Gets the Raspi's default gateway
     with open("/proc/net/route") as fh:
         for line in fh:
             fields = line.strip().split()
@@ -36,7 +37,7 @@ def get_default_gateway(): #Gets the Raspi's default gateway
             return socket.inet_ntoa(struct.pack("<L", int(fields[2], 16)))
 
 
-def get_iface_speed(iface): #Gets the reported speed of the interface the Raspi is connected to. Should only work to 1 Gb/s because of limitations on the pi's interface
+def get_iface_speed(iface):  # Gets the reported speed of the interface the Raspi is connected to.
     try:
         speed = subprocess.getstatusoutput("ethtool " + iface + " | grep Speed")
         speed = str(speed[1])
@@ -47,7 +48,7 @@ def get_iface_speed(iface): #Gets the reported speed of the interface the Raspi 
         return "N/A"
 
 
-def get_ifaceupdown(iface): #Gets the status of the internet interface
+def get_ifaceupdown(iface):  # Gets the status of the internet interface
     i = subprocess.getstatusoutput("cat /sys/class/net/" + iface + "/operstate")
     i = i[1]
     if i != "down":
@@ -56,7 +57,7 @@ def get_ifaceupdown(iface): #Gets the status of the internet interface
         return "Down"
 
 
-def ping_host(host, number): #Host should be an IP address. Pings a host a number of time and gets the output in a string once it is completed
+def ping_host(host, number):  # Host should be an IP address. Pings a host a number of time and gets the output
     number = str(number)
     print("Starting ping. This may take a minute...")
     ping = subprocess.getstatusoutput("ping -c " + number + " " + host)
@@ -64,7 +65,7 @@ def ping_host(host, number): #Host should be an IP address. Pings a host a numbe
     return ping[1]
 
 
-def check_dns(host): #Host should be a domain name. Returns true if the Raspi can access that domain
+def check_dns(host):  # Host should be a domain name. Returns true if the Raspi can access that domain
     print("Checking DNS. May take a minute...")
     result = subprocess.getstatusoutput("ping -c 1 " + host)
     if result[0] == 0:
@@ -74,7 +75,7 @@ def check_dns(host): #Host should be a domain name. Returns true if the Raspi ca
         return False
 
 
-def get_internet_speed(): #Checks connectivity to speedtest.net. Then output the upload and download speed in a string
+def get_internet_speed():  # Checks connectivity to speedtest.net. Then output the upload and download speed in a string
     result = subprocess.getstatusoutput("ping -c 2 speedtest.net")
     if result[0] != 0:
         print("Could not connect. Retrying...")
@@ -92,8 +93,9 @@ def get_internet_speed(): #Checks connectivity to speedtest.net. Then output the
     return ("Download: %s Mb/s, Upload: %s Mb/s" %(down, up))
 
 
-def dhcp_reload(iface):
+def dhcp_reload(iface):  # Requests a new DHCP lease
     rel = subprocess.getstatusoutput("dhclient -r %s && sudo dhclient %s" %(iface, iface))
+    print("Reloading dhcp...")
 
     return rel[1]
 
---
+++ EtherView/main.py
@@ -0,0 +1,79 @@
+import lib
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog as fd
+from tkinter import messagebox
+
+defbgn = "#EEf0F1" # Default background color
+dnshost = "google.com"
+iface = "bond0"  # Will be later eth0 when on the raspberrypi
+
+
+def do_ping():  # INCOMPLETE FOR NOW!!!
+    print("Opening Ping...")
+    ping_window = Toplevel(height=400, width=400)
+    ping_window.focus_get()
+    # for i in ["7", "8", "9", "4", "5", "6", "1", "2", "3", ".", "0", "Enter"]:
+    ping_label = ttk.Label(ping_window, text="Enter host to ping",background=defbgn)
+    ping_input = ttk.Entry(ping_window)
+    ping_label.place(x=210, y=10, height=30)
+    ping_input.place(x=210, y=45, width=160, height=30)
+
+
+def check_dns():  # Animates the button when press and displays info
+    dns_button.config(state="disabled")
+    response = lib.check_dns(dnshost)
+
+    while response is not True or not False:
+        if response is True:
+            messagebox.showinfo("Success", "DNS is connected")
+            dns_button.config(state="normal")
+            return True
+        if response is False:
+            messagebox.showwarning("Failure", "DNS does not appear to be connected!")
+            dns_button.config(state="normal")
+            return False
+        #else:
+            #print("Waiting for test...")
+
+
+def dhcp_reload():
+    lib.dhcp_reload(iface)
+
+
+def check_connexion():
+    ip_address = "--"
+    netmask = "__"
+    default = "--"
+    link = lib.get_ifaceupdown(iface)
+    if link is "Up":
+        ip_address = lib.get_ip(iface)
+    if link is "Up":
+        netmask = lib.get_netmask(iface)
+    if link is "Up":
+        default = lib.get_default_gateway()
+
+    out = "Interface: %s\n\nLink: %s\n\nIP Address: %s\n\nNetmask: %s\n\nDefault Gateway: %s" %(iface, link, ip_address, netmask, default)
+    ip_info.config(text=out, anchor="nw")
+    root.after(1000, check_connexion)
+
+
+root = Tk()
+root.title("EtherView Analyzer")
+root.focus_force()
+root.geometry("800x480")
+# root.attributes("-fullscreen", True) # Uncomment to put fullscreen
+
+ip_info = ttk.Label(root, font=("FreeSans", 20))
+
+ping_button = Button(root, text="Ping", command=do_ping, background="orange red")
+dns_button = Button(root, text="DNS Test", command=check_dns, background="lime green")
+dhcp_button = Button(root, text="Reload DHCP", command=dhcp_reload, background="deep sky blue")
+
+ip_info.place(x=10, y=10, width=380, height=380)
+ping_button.place(x=470, y=10, width=150, height=150)
+dns_button.place(x=630, y=10, width=150, height=150)
+dhcp_button.place(x=470, y=170, width=150, height=150)
+
+check_connexion()
+root.mainloop()
